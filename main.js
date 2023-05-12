a1205a47a5ee9ea1e79ebfbef19e287e6eacb6da
beekay19 <bknelson19@gmail.com>
2023-05-12 05:28:37 +0100

    first commit

---
+++ main.js
@@ -0,0 +1,59 @@
+const theme1 = document.getElementById("theme1");
+const theme2 = document.getElementById("theme2");
+const theme3 = document.getElementById("theme3");
+const keys = document.querySelectorAll(".key");
+const screen = document.querySelector(".display");
+const reset = document.querySelector(".reset");
+const del = document.querySelector(".del");
+const equal = document.querySelector(".equal");
+
+keys.forEach(function (key) {
+  key.addEventListener("click", function (e) {
+    let val = e.target.innerText;
+    screen.value += val;
+    // if(e.target.innerText === '*'){
+    //     console.log(e)
+    //     // e.target.innerText = 'x'
+    // }
+    
+  });
+});
+
+theme1.addEventListener("click", function () {
+  document.body.classList.remove("theme-1");
+  document.body.classList.remove("theme-2");
+});
+theme2.addEventListener("click", function () {
+  document.body.classList.add("theme-1");
+  document.body.classList.remove("theme-2");
+});
+theme3.addEventListener("click", function () {
+  document.body.classList.remove("theme-1");
+  document.body.classList.add("theme-2");
+});
+
+reset.addEventListener("click", function () {
+  screen.value = "";
+});
+
+del.addEventListener("click", function () {
+  let inputField = screen.value;
+  inputField = inputField.slice(0, -1);
+  screen.value = inputField;
+});
+
+let calculator;
+calculator = 22.2 / 2;
+
+// console.log(eval("24+23"));
+equal.addEventListener('click',function (e) {
+    let sumX =  screen.value
+    
+    if(eval(sumX) == null){
+        alert('please enter a number')
+    }else{
+        screen.value = eval(sumX)
+        // console.log(typeof eval(sumX))
+    }
+})
+
